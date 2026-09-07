bfdd2f64a7313e5047dcb0ed3e0657006f2c17ea
cloudswebnepal8 <cloudswebnepal8@gmail.com>
2026-09-07 12:50:53 +0545

    file

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "https://anugraha-backends.vercel.app/",
+    baseURL: "https://anugraha-backends.vercel.app",
     headers: {
         "Content-Type": "application/json"
     }
